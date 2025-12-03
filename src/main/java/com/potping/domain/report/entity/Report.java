de50547fcbf1f545fabf2434533fcd2c17b1a1b7
eastminnn <oh1334000@gmail.com>
2025-12-03 23:57:12 +0900

    feat : 신고 엔티티 추가

---
+++ src/main/java/com/potping/domain/report/entity/Report.java
@@ -0,0 +1,52 @@
+package com.potping.domain.report.entity;
+
+import com.potping.domain.pothole.entity.Pothole;
+import com.potping.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Report {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "report_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private ReportProcessStatus processStatus;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime reportedAt; // 신고 시각
+
+    private LocalDateTime completedAt; // 보수 완료 시각
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pothole_id", nullable = false)
+    private Pothole pothole; // 어떤 포트홀에 대한 신고인가
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User admin; // 처리한 관리자
+
+    @Builder
+    public Report(Pothole pothole, User admin) {
+        this.pothole = pothole;
+        this.admin = admin;
+        this.processStatus = ReportProcessStatus.SUBMITTED;
+    }
+
+    public void complete() {
+        this.processStatus = ReportProcessStatus.DONE;
+        this.completedAt = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/potping/domain/report/entity/ReportProcessStatus.java
@@ -0,0 +1,6 @@
+package com.potping.domain.report.entity;
+
+public enum ReportProcessStatus {
+    SUBMITTED, // 신고 접수
+    DONE // 보수 완료
+}
