b538f393bc2ab0de8bb49167aa071374fa65275d
eastminnn <oh1334000@gmail.com>
2025-12-03 14:19:48 +0900

    chore : 초기 환경세팅

---
+++ src/main/java/com/potping/PotpingApplication.java
@@ -0,0 +1,13 @@
+package com.potping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotpingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotpingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/potping/PotpingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotpingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
